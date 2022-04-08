108c7abd415afcee29c03f29e1c165d2c2f709ef
lumpenop <swk5373@gmail.com>
2022-04-08 21:12:01 +0900

    Feat: hanspell 추가

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
 from flask_cors import CORS ## 설치는 flask-cors 
+from hanspell import spell_checker
+
 
 app = Flask(__name__)
 CORS(app)
@@ -18,8 +20,10 @@ def result():
         params = request.json
         if len(params) == 0:
             return 'no prams'
-
-        print(params['data'])
+        
+        spelled_sent = spell_checker.check(params['data'])
+        checked_sent = spelled_sent.checked
+        prams['data'] = checked_sent
         return jsonify(params)
 
 
--- static/index.js
+++ static/index.js
@@ -1,10 +1,20 @@
 
 class App {
   init() {
-    document
-      .querySelector('.button')
-      .addEventListener('click', this.spellCheck.bind(this));
+    const button = document.querySelector('.button');
+    const input = document.querySelector('.input-text')
+    button.addEventListener('click', this.spellCheck.bind(this));
+
+    const spell = this.spellCheck.bind(this);
+    input.addEventListener('keypress', 
+    function (e) {
+      if(e.key === 'Enter') {
+        spell();
+      }
+    })   
+    
   }
+  
   post(body) {
     fetch(`http://localhost:5000/result`, {
       method: 'POST',
@@ -19,13 +29,12 @@ class App {
   }
   spellCheck() {
     const text = document.querySelector('.input-text').value;
-    this.post(text);
+    if(text !== '') this.post(text);
   }
 
   render(response) {
     const resultWrapper = document.querySelector('.response-wrapper');
     document.querySelector('.input-text').value = '';
-    console.log(response);
     resultWrapper.innerHTML = response.data;
   }
 }
